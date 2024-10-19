ded042ae01ec9ff8075298460c6a6a968bbb556f
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-10-19 12:26:15 -0700

    jkd
    mjdhg

--- USACO1/Phil/Array.py
+++ USACO1/Phil/Array.py
@@ -4,3 +4,10 @@ What are the indices of the elements "Apple", "Bread", and "Milk"?
 python
 food = ["Apple", "Shrimp", "Pineapple", "Bread", "Eggs", "Milk"]
 '''
+food = ["Apple", "Shrimp", "Pineapple", "Bread", "Eggs", "Milk"]
+
+lookfor= ["Apple", "Bread", "Milk"]
+indix = []
+for i in range(len(food)):
+    if food[i] in lookfor:
+        indix.append(i)
---
+++ USACO1/Phil/Questions.py
@@ -0,0 +1,8 @@
+'''
+Problem 5
+What is the problem with the following code?
+
+python
+names = ["Kevin", "Chris", "Mayami"]
+print(names[10])
+'''
