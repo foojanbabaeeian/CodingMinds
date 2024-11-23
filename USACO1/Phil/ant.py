a2f14648df045a8c28882ab628ef74f1c4eb01d6
nextbanboo <nextbanboo@gmail.com>
2024-11-23 12:36:38 -0800

    Update ant.py

--- USACO1/Phil/ant.py
+++ USACO1/Phil/ant.py
@@ -1,11 +1,26 @@
-def gay(x,n):
-    if x**2==n:
-        print("True")
-        return
-    elif x>n/2:
-        print("False")
-        return
+# def gay(x,n):
+#     if x**2==n:
+#         print("True")
+#         return
+#     elif x>n/2:
+#         print("False")
+#         return
+#     else:
+#         x+=1
+#         gay(x,n)
+# gay(1,9)
+def fizz(x,y):
+    if type(x/3)== int:
+        if type(x/5)== int:
+            print("Fizz Buzz")
+        else:
+            print("Fizz")
+    elif type(x/5)== int:
+        print("Buzz")
     else:
+        print(x)
         x+=1
-        gay(x,n)
-gay(1,9)
+        if x==50:
+            print(x)
+            return
+fizz(1,50)
