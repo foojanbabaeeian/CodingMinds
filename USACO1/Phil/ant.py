fcec8f7802a01c31d160002c89904ef3a3c4c755
nextbanboo <nextbanboo@gmail.com>
2024-11-23 13:06:21 -0800

    Update ant.py

--- USACO1/Phil/ant.py
+++ USACO1/Phil/ant.py
@@ -1,42 +1,46 @@
-# def gay(x,n):
-#     if x**2==n:
-#         print("True")
-#         return
-#     elif x>n/2:
-#         print("False")
-#         return
-#     else:
+# # def gay(x,n):
+# #     if x**2==n:
+# #         print("True")
+# #         return
+# #     elif x>n/2:
+# #         print("False")
+# #         return
+# #     else:
+# #         x+=1
+# #         gay(x,n)
+# # gay(1,9)
+# def fizz(x,y):
+#     if x%3 ==0:
+#         if x%5 ==0:
+#             print("Fizz Buzz")
+#             x+=1
+#             if x==y:
+#                 print("Fizz Buzz")
+#                 return
+#             fizz(x,y)
+#         else:
+#             print("Fizz")
+#             x+=1
+#             if x==y:
+#                 print("Fizz")
+#                 return
+#             fizz(x,y)
+#     elif x%5 ==0:
+#         print("Buzz")
 #         x+=1
-#         gay(x,n)
-# gay(1,9)
-def fizz(x,y):
-    if x%3 ==0:
-        if x%5 ==0:
-            print("Fizz Buzz")
-            x+=1
-            if x==y:
-                print("Fizz Buzz")
-                return
-            fizz(x,y)
-        else:
-            print("Fizz")
-            x+=1
-            if x==y:
-                print("Fizz")
-                return
-            fizz(x,y)
-    elif x%5 ==0:
-        print("Buzz")
-        x+=1
-        if x==y:
-            print("Buzz")
-            return
-        fizz(x,y)
-    else:
-        print(x)
-        x+=1
-        if x==y:
-            print(x)
-            return
-        fizz(x,y)
-fizz(1,50)
+#         if x==y:
+#             print("Buzz")
+#             return
+#         fizz(x,y)
+#     else:
+#         print(x)
+#         x+=1
+#         if x==y:
+#             print(x)
+#             return
+#         fizz(x,y)
+# fizz(1,50)
+s1 ="noob"
+s2 ="boon"
+if s1.__len__==s2.__len__:
+    print("ture")
