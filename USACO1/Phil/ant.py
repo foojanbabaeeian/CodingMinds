827343b07aa3e4e6f888ff687b4b18c1fd1d0334
nextbanboo <nextbanboo@gmail.com>
2024-11-23 12:27:06 -0800

    Update ant.py

--- USACO1/Phil/ant.py
+++ USACO1/Phil/ant.py
@@ -1,9 +1,11 @@
-# chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://physics.montana.edu/avorontsov/teaching/problemoftheweek/documents/problem_Math004.pdf
-'''
-蚂蚁在尺子上
-
-一百只蚂蚁被放在一根一米长的尺子上。每只蚂蚁都以每分钟 1 米的恒定速度朝左或朝右行进。当两只蚂蚁相遇时，它们会互相弹开并反向行进。当一只蚂蚁到达尺子的一端时，它会掉下去。最终，所有的蚂蚁都会掉下尺子。这个过程所需的时间取决于蚂蚁的初始配置。
-
-问题： (a) 在所有可能的初始配置中，确保尺子上没有蚂蚁所需的最长时间是多少？ (b) 一个稍微技术一点的任务：计算任意初始蚂蚁数量 N 掉下尺子的平均时间。
-'''
-
+def gay(x,n):
+    if x**2==n:
+        print("True")
+        return
+    elif x>n/2:
+        print("False")
+        return
+    else:
+        x+=1
+        gay(x,n)
+gay(1,9)
