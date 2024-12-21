278985a2496df705e68f087aa8e5e8d180102801
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-12-21 15:12:16 -0800

    questions 12/21
    ADDING QUESTIONS to the question folder

---
+++ USACO1/Phil/Questions/Q12.py
@@ -0,0 +1,15 @@
+'''
+Practice Question 12: Factorial of number N
+A factorial is the product of all whole numbers from the chosen number down to 1. Find the factorial of a given integer n. Assume the given integer n is guaranteed to be greater than 0.
+
+Example:
+
+python
+# output: 24
+# 4 factorial = 4 x 3 x 2 x 1 = 24
+print(factorial(4))
+
+# output: 5040
+# 4 factorial = 7 x 6 x 5 
+'''
+print("Hi")
---
+++ USACO1/Phil/Questions/Q13.py
@@ -0,0 +1,18 @@
+'''
+Practice Question 13: Longest Word in a List
+Given a list of strings, find all of the longest words in the list. There may be multiple words that have the same length.
+
+Example:
+
+python
+words = ["Hello", "pizza", "chocolate", "wrench"]
+
+# output: ["chocolate"]
+print(longestWord(words))
+
+
+names = ["James", "Joy", "Rick", "John", "Kevin", "Jay"]
+
+# output: ["James", "Kevin"]
+print(longestWord(names))
+'''
