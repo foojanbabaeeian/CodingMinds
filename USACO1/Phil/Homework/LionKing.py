f2ac10d0fbcf0b244e97efd6bc6387ae6496e969
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2024-12-21 16:05:18 -0800

    homework
    Homework for next session

---
+++ USACO1/Phil/Homework/LionKing.py
@@ -0,0 +1,51 @@
+'''
+Practice Question 15: Upper Quartile GPA
+The upper quartile, or third quartile, is the value that separates the top 25% with the lower 75% in a dataset. Given a list of numbers representing every students' grade, find the upper quartile GPA in the class among all students.
+
+Example:
+
+python
+gpa = [3.5, 2.5, 3.7, 3.14, 3.42]
+
+# output: 3.5
+print(upperQuartile(gpa))
+'''
+
+'''
+Practice Question 16: Count Non Unique Numbers
+Count how many numbers that has appeared two or more times in a given list.
+
+Example 1:
+
+python
+nums = [8, 1, 10, 1, 2, 2, 8, 7, 10, 3]
+
+# output: 4
+# number 1, 2, 8, and 10 has appeared two or more times
+print(countNonUnique(nums))
+Example 2:
+
+python
+nums = [2, 5, 10, 7, 6, 2, 7, 5, 10, 2, 6, 10]
+
+# output: 5
+# number 2, 5, 6, 7, and 10 has appeared two or more times
+print(countNonUnique(nums))
+
+'''
+
+'''
+Practice Question 17: Maximum of Three Numbers
+Write a function that finds the maximum of the three numbers. Try not to use Python's built in functions such as sort().
+
+Example 1:
+
+python
+# output: 15
+print(max(3, 15, 9))
+Example 2:
+
+python
+# output: 0
+print(max(-1, -4, 0))
+'''
