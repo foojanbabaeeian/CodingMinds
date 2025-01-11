b7f9ba32c98d05dd87b2444b38a76ce64eb51ffe
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2025-01-11 12:34:32 -0800

    push

---
+++ USACO1/Phil/Questions/q17.py
@@ -0,0 +1,19 @@
+Practice Question 16: Count Non Unique Numbers
+Count how many numbers that has appeared two or more times in a given list.
+
+Example 1:
+
+python
+nums = [8, 1, 10, 1, 2, 2, 8, 7, 10, 3]
+
+# output: 4
+# number 1, 2, 8, and 10 has appeared two or more times
+print(countNonUnique(nums))
+Example 2:
+
+python
+nums = [2, 5, 10, 7, 6, 2, 7, 5, 10, 2, 6, 10]
+
+# output: 5
+# number 2, 5, 6, 7, and 10 has appeared two or more times
+print(countNonUnique(nums))
