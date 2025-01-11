b941753894a2837ae1addef336011be84f84bfa8
nextbanboo <nextbanboo@gmail.com>
2025-01-11 12:56:47 -0800

    Update q17.py

--- USACO1/Phil/Questions/q17.py
+++ USACO1/Phil/Questions/q17.py
@@ -1,19 +1,33 @@
-Practice Question 16: Count Non Unique Numbers
-Count how many numbers that has appeared two or more times in a given list.
+# Practice Question 16: Count Non Unique Numbers
+# Count how many numbers that has appeared two or more times in a given list.
 
-Example 1:
+# Example 1:
 
-python
-nums = [8, 1, 10, 1, 2, 2, 8, 7, 10, 3]
+# python
+# nums = [8, 1, 10, 1, 2, 2, 8, 7, 10, 3]
 
-# output: 4
-# number 1, 2, 8, and 10 has appeared two or more times
-print(countNonUnique(nums))
-Example 2:
+# # output: 4
+# # number 1, 2, 8, and 10 has appeared two or more times
+# print(countNonUnique(nums))
+# Example 2:
 
-python
+# python
+# nums = [2, 5, 10, 7, 6, 2, 7, 5, 10, 2, 6, 10]
+
+# # output: 5
+# # number 2, 5, 6, 7, and 10 has appeared two or more times
+# print(countNonUnique(nums))
 nums = [2, 5, 10, 7, 6, 2, 7, 5, 10, 2, 6, 10]
-
-# output: 5
-# number 2, 5, 6, 7, and 10 has appeared two or more times
-print(countNonUnique(nums))
+gay=[]
+gayy=0
+for num in nums:
+    if num in gay:
+        print("boo")
+    else:
+        if gay.count(num)>= 1:
+            gayy+=1
+            gay.append(num)
+        else:
+            pass
+    print(gay, gayy)
+print(gayy)
