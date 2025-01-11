a5eb5ec51fcb8a314391b4754754b85c370c6461
Fozhan Babaeiyan <babaeeianwork@gmail.com>
2025-01-11 13:05:52 -0800

    Create Homework.py

---
+++ USACO1/Phil/Questions/Homework.py
@@ -0,0 +1,29 @@
+'''
+Practice Question 17: Maximum of Three Numbers
+Write a function that finds the maximum of the three numbers. Try not to use Python's built in functions such as sort().
+
+Example 1:
+
+python
+# output: 15
+print(max(3, 15, 9))
+Example 2:
+
+python
+# output: 0
+print(max(-1, -4, 0))
+
+Practice Question 18: Rounding Numbers
+Write a function that rounds a number to the 2nd place. Try not to use Python's built in functions such as round().
+
+Example 1:
+
+python
+# output: 3.14
+print(roundNum(3.1415926))
+Example 2:
+
+python
+# output: 2.72
+print(roundNum(2.718281828))
+'''
