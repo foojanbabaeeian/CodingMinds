835f9f89e7dce12e47c60442cbd9278b2fcd56e0
Foojanb <babaeeianwork@gmail.com>
2025-02-08 12:42:39 -0800

    Update questions.py

---
+++ USACO1/Phil/usaco2/sets/questions.py
@@ -0,0 +1,25 @@
+'''
+P3-3 Exercise: Count Unique Characters
+Problem Statement
+Given a string, count how many unique characters appear in this string, and return the number.
+
+Can you solve it in a linear time complexity O(n)? Hint: Use a dictionary instead of nested loops.
+
+Example 1:
+
+python
+word = "ssahua"
+
+# output: 4
+# The four unique characters are "s", "a", "h", "u"
+print(countChar(word))
+Example 2:
+
+python
+word = "kk"
+
+# output: 1
+# The unique character is "k"
+print(countChar(word))
+
+'''
