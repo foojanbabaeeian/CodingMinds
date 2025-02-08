a813ac14f7fa02dd95679ea40e13eaa673a47b90
Foojanb <babaeeianwork@gmail.com>
2025-02-08 12:17:17 -0800

    changed the folders

---
+++ USACO1/Phil/usaco1/usaco2/dictionary/main.py
@@ -0,0 +1,13 @@
+'''
+Problem 5
+
+Write a program that stores all keys in a dictionary into a list.
+
+Example:
+
+python
+dict = { "Andrew": 27, "Kevin": 35, "Rapi": 18, "Dola": 29 }
+
+# expected values in the list
+keys = ["Andrew", "Kevin", "Rapi", "Dola"]
+'''
