3b2e146d327d8059b58978a80a97d71423976710
nextbanboo <nextbanboo@gmail.com>
2025-03-15 14:26:50 -0700

    Update ant.py

--- USACO/Phil/usaco2/Stacks and Queues/ant.py
+++ USACO/Phil/usaco2/Stacks and Queues/ant.py
@@ -1,21 +1,37 @@
-p = "{()}[]"
-gay=[]
-def val():
-    for i in p:
-        if i == '[' or i == '(' or i =='{':
-            gay.append(i)
-            print(gay)
-        if len(gay)>0:
-            if i == ']' and gay[-1] == '[':
-                gay.pop()
-            elif i == '}' and gay[-1] == '{':
-                gay.pop()
-            elif i == ')' and gay[-1] == '(':
-                gay.pop()
-    if gay == []:
-        return True
+# p = "{()}[]"
+# gay=[]
+# def val():
+#     for i in p:
+#         if i == '[' or i == '(' or i =='{':
+#             gay.append(i)
+#             print(gay)
+#         if len(gay)>0:
+#             if i == ']' and gay[-1] == '[':
+#                 gay.pop()
+#             elif i == '}' and gay[-1] == '{':
+#                 gay.pop()
+#             elif i == ')' and gay[-1] == '(':
+#                 gay.pop()
+#     if gay == []:
+#         return True
+#     else:
+#         print("no")
+# val()
+s = "abc#c"
+t = "abbc"
+gay = []
+gays =[]
+for i in s:
+    if i == "#":
+        gay.pop()
     else:
-        print("no")
-val()
-
-            
+        gay.append(i)
+for i in t:
+    if i == "#"
+        gays.pop()
+    else:
+        gays.append(i)
+if gay == gays:
+    print("true")
+else:
+    print("false")
