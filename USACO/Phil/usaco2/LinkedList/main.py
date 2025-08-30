a3bf6bfc80b3db02966a71405376695805b646e3
nextbanboo <nextbanboo@gmail.com>
2025-08-30 13:24:13 -0700

    Update main.py

--- USACO/Phil/usaco2/LinkedList/main.py
+++ USACO/Phil/usaco2/LinkedList/main.py
@@ -1,2 +1,13 @@
 guys = ["Phil", "alvin"]
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class linkedlist:
+    def __init__(self):
+        self.head = None
 
+    def push(self,newData):
+        newNode=Node(newData)
+        newNode.next = self.head
+        self.head = newNodex
