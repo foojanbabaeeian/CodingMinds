4a3ec777f684583a08504deedf05f6df5503c982
Foojanb <79134873+foojanbabaeeian@users.noreply.github.com>
2025-09-28 10:48:55 -0700

    added the collision

--- Python3/Folder/s2.py
+++ Python3/Folder/s2.py
@@ -11,35 +11,58 @@ pygame.mixer.music.load("The Duck Song.mp3")
 
 pygame.mixer.music.play(1)
 
-class Albert(pygame.sprite.Sprite):
-    def __init__(self, image):
+
+
+class Alberta(pygame.sprite.Sprite):
+    def __init__(self, image, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = image
+        width = image.get_width()  
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * 0.1), int(height * 0.1)))
         self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
     def update(self):
         keystate = pygame.key.get_pressed()
-
+        
         if keystate[pygame.K_UP]:
             self.rect.y -= 5
         elif keystate[pygame.K_DOWN]:
             self.rect.y += 5
-            
-class Kai(pygame.sprite.Sprite):
-    def __init__(self, image):
+
+
+
+class Kaitherine(pygame.sprite.Sprite):
+    def __init__(self, image, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = image
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * 0.5), int(height * 0.5)))
         self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
     def update(self):
-        self.rect.y += 5
+        self.rect.x += 5
         if self.rect.left > 500:
             self.rect.right = 0
 
 kai_image = pygame.image.load("kai.png")
 albert_image = pygame.image.load("albert.png")
 
+# I would like you to change where alberta and kai start
+
+albert
+Albertata = Alberta(albert_image)
+Alberta_group = pygame.sprite.Group()
+Alberta_group.add(Albertata)
 
 
+kai = Kaitherine(kai_image)
+kai_group = pygame.sprite.Group()
+kai_group.add(kai)
+
+#  :D
 while True:
     
     screen.fill((0,0,0))
@@ -48,10 +71,21 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+    Alberta_group.draw(screen)
+    Alberta_group.update()
 
-    
+    kai_group.draw(screen)
+    kai_group.update()
 
-
-    
     pygame.display.update()
+    # save list of collided sprites into a variable
+    hit_list = pygame.sprite.spritecollide(Albertata, kai_group, False)
+
+    # iterate through list
+    for hit in hit_list:
+        print("Collided!")
+
+    # if pygame.sprite.collide_rect(Albertata, kai):
+    #     print("You Suck")
+
     clock.tick(60)
